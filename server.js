50a66523c4dd18c337dc1db13785742fd232fb6b
ncard <ncardenas101903@gmail.com>
2023-09-04 09:36:04 -0500

    ALgunos archivos extra

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router()
 
 var app = express();
-
+app.use(bodyParser)
 app.use(router)
 
 router.get('/message', (re1, res) => {
     res.send("Lista de mensajes")
 })
 
-router.post('/message', (re1, res) => {
+router.delete('/message', (re1, res) => {
+    console.log(req.body);
     res.send("Mensaje añadido")
 })
-
-// app.use('/', (req, res) => {
-//     res.send('Hola')
-// })
 
 app.listen(3000)
 console.log('Listening: 3000');
