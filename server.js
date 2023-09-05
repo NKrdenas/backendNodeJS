64cb1c947da69fa2cad60c66cfc4a1d637929f5c
ncard <ncardenas101903@gmail.com>
2023-09-05 11:48:07 -0500

    Cambios en server.js

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const bodyParser = require('body-parser');
 const router = express.Router()
 
 var app = express();
-app.use(bodyParser)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(router)
 
-router.get('/message', (re1, res) => {
+router.get('/message', (req, res) => {
+    console.log(req.headers);
+    res.header({
+        "custom-header": "Valor personalizado"
+    })
     res.send("Lista de mensajes")
 })
 
-router.delete('/message', (re1, res) => {
+router.delete('/message', (req, res) => {
+    console.log(req.query);
     console.log(req.body);
-    res.send("Mensaje añadido")
+    res.status(201).send({error: "", body: 'Creado Correctamente'})
 })
 
 app.listen(3000)
