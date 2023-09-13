dbcaad2d1d344a6e51eeb7c0a647d814aa9aeef1
ncard <ncardenas101903@gmail.com>
2023-09-12 23:34:36 -0500

    Conectamos a la base de datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,14 +1,33 @@
-const list = []
+const db = require('mongoose')
+const Model = require('./model');
+const username = encodeURIComponent("<UsuarioDefinitivo>")
+const password = encodeURIComponent("<l5n9SRYeKfGtEw1h>")
+
+//Ponemos la uri en un archivo "uri.js, lo exportamos y añadimos a un .gitignore
+const uri = require('../../uri')
+
+db.Promise = global.Promise
+db.connect(uri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'backendNodeJS'
+})
+    .then(() => console.log('[db]: Conectada con exito'))
+    .catch(err => console.error('[db]: ', err))
 
 function addMesage(message){
-    list.push(message)
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessage(){
-    return list
+async function getMessage(){
+    const messages = await Model.find()
+    return messages
 }
 
 module.exports = {
     add: addMesage,
     list:  getMessage
 }
+
+//l5n9SRYeKfGtEw1h: UsuarioDefinitvo
