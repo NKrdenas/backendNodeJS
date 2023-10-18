f80fcb7faf41028cd7d2c270876d2188549c9410
ncard <ncardenas101903@gmail.com>
2023-10-18 14:32:42 -0400

    Conectamos la api con websockets

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const app = express();
+const server = require('http').Server(app)
 
+const bodyParser = require('body-parser');
+const socket = require('./socket')
 const db = require('./db');
 const uri = require('./uri');
 // const router =  require('./components/message/network');
@@ -8,14 +11,15 @@ const router = require('./network/routes')
 
 db(uri)
 
-var app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-// app.use(router)
+
+socket.connect(server)
 
 router(app);
 
 app.use('/app', express.static('public'))
 
-app.listen(3000)
-console.log('Listening: 3000');
+app.listen(3000, () => {
+    console.log('Listening: 3000')
+})
